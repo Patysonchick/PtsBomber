5d3158221285443867c422a79adc8092e7e9ea14
Эдгар Томский <101949701+Patysonchick@users.noreply.github.com>
2023-07-19 11:47:19 +0300

    Обновление v1.0
    Был переработан весь бомбер и добавлено побольше сервисов.

---
+++ libs.hpp
@@ -0,0 +1,13 @@
+/*
+Список библиотек
+
+Если у возникает ошибка по типу: "#include <curl/curl.h> не найдена акая библиотека",
+то надо установить libcurl для C/C++, например: sudo apt install libcurl4-openssl-dev
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <curl/curl.h>
+
+using namespace std;
